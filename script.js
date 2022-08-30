e28e6f765a18c063a042016911d8e6e932a3e258
Bitaw <stickynotes5@yahoo.com>
2022-08-30 15:45:41 -0500

    Added new API call for 5 Day Forecast to script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // fetch(
-//   "http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=e57070c21ce9424475afa701ad71a404"
+//   "https://api.openweathermap.org/data/2.5/forecast?id=524901&appid=e57070c21ce9424475afa701ad71a404"
 // )
 //   .then((response) => {
 //     return response.json();
@@ -19,20 +19,28 @@
 //     console.log(users);
 //   });
 
-// //5 Day Forecast
-// fetch(
-//   "api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=e57070c21ce9424475afa701ad71a404"
-// )
-//   .then((response) => {
-//     return response.json();
-//   })
-//   .then((users) => {
-//     console.log(users);
-//   });
+// 5 Day Forecast
+const fiveDayForecast = function(city){// Make an object here
+ fetch(
+   "api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=e57070c21ce9424475afa701ad71a404"
+)
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    console.log(data);
+    for(i=0; i<data.list.length; i++){
+      const { name } = data.city.name;
+      const { icon } = data.weather[0];
+      const { temp, humidity } = data.list.main;
+      const { speed } = data.wind;
+      console.log(name, icon, temp, humidity, speed)
+    }
+  });}
 
 //use OneCall api 3.0 instead?
 let weather = {
-  apiKey: "",
+  apiKey: "e57070c21ce9424475afa701ad71a404",
   fetchWeather: function (city) {
     fetch(
       "https://api.openweathermap.org/data/2.5/weather?q=" +
@@ -55,20 +63,32 @@ let weather = {
     document.querySelector(".humidity").innerText =
       "Humidity: " + humidity + "%";
   },
-  fetch5Day: function (city) {
-    fetch(
-      "api.openweathermap.org/data/2.5/forecast?lat=" +
-        lat +
-        "&lon=" +
-        lon +
-        "&appid=e57070c21ce9424475afa701ad71a404"
-    )
-      .then((response) => response.json())
-      .then((data) => this.display5Day(data));
-  },
-  display5Day: function (data) {
+ search: function() {
+  this.fetchWeather(document.querySelector("#searchCity").value)
+ }
+}
 
-    const 
+document.querySelector("#searchForm").addEventListener("submit", function(event) {event.preventDefault()
+  console.log(document.querySelector("#searchCity").value)
+   weather.search()
+})
 
-  },
-};
+//for loop
+// const temp = data.list[0].main.temp;
+
+
+ // fetch5Day: function (city) {
+  //   fetch(
+  //     "api.openweathermap.org/data/2.5/forecast?lat=" +
+  //       lat +
+  //       "&lon=" +
+  //       lon +
+  //       "&appid=e57070c21ce9424475afa701ad71a404"
+  //   )
+  //     .then((response) => response.json())
+  //     .then((data) => this.display5Day(data));
+  // },
+  // display5Day: function (data) {
+  //   const { temp, humidity } = data.list[0].main;
+  // },
+
