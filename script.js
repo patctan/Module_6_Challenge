be66fcb054f64ebec90ea219ebdd7855c061b3dd
Bitaw <stickynotes5@yahoo.com>
2022-09-06 18:25:33 -0500

    Added function to dynamically create cards for 5 day forecast in script.js

--- script.js
+++ script.js
@@ -19,26 +19,51 @@
 //     console.log(users);
 //   });
 
-// 5 Day Forecast
+// const { name } = data.city.name;
+// const { icon } = data.weather[0];
+// const { temp, humidity } = data.list.main;
+// const { speed } = data.wind;
+// console.log(name, icon, temp, humidity, speed)
+
+//1. Push data from for loop from fetch request into an array, which is set to a variable
+//2. Make a function to dynamically create cards
+//3. Use map method to update array for the next card, etc.?
+// 06-Server-side-APIs, Activity 03
+
+
+// function createCards(){
+//  document.createElement("div");
+
+// }
+
+// // 5 Day Forecast
+
+ var fiveDayArray = []; 
+ console.log(fiveDayArray)
+
 const fiveDayForecast = function(city){// Make an object here
+  console.log("https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&appid=e57070c21ce9424475afa701ad71a404"
+  )
  fetch(
-   "api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=e57070c21ce9424475afa701ad71a404"
+   "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&appid=e57070c21ce9424475afa701ad71a404"
 )
   .then((response) => {
     return response.json();
   })
   .then((data) => {
     console.log(data);
-    for(i=0; i<data.list.length; i++){
-      const { name } = data.city.name;
-      const { icon } = data.weather[0];
-      const { temp, humidity } = data.list.main;
-      const { speed } = data.wind;
-      console.log(name, icon, temp, humidity, speed)
-    }
-  });}
+    for(i=0; i<data.list.length; i+=8){
+      fiveDayArray.push(data.list[i])
+      
+     }
+    console.log(data.list[0])
+    console.log(data.list[0].main.humidity)
+    console.log(data.list[0].wind.speed)
+    console.log(data.list[0].main.temp)
+    });}
+fiveDayForecast('manila');
 
-//use OneCall api 3.0 instead?
+
 let weather = {
   apiKey: "e57070c21ce9424475afa701ad71a404",
   fetchWeather: function (city) {
@@ -67,28 +92,32 @@ let weather = {
   this.fetchWeather(document.querySelector("#searchCity").value)
  }
 }
+var fiveDayPullInfo = ``
 
+function generateCards(){
+  fiveDayArray.forEach(function(data){
+  
+    fiveDayPullInfo = fiveDayPullInfo + `
+    <card
+    class = "card">
+    <article>
+    <div>
+    <img src = "http://openweathermap.org/img/w/${data.weather[0].icon}.png";>
+    </div>
+    <div>
+    <p>Temp: ${data.main.temp}</p>
+    <p>Wind Speed: ${data.wind.speed}</p>
+    <p>Humidity: ${data.main.humidity}</p>
+    </div>
+    </article></card>`;})
+    
+    const fiveDayCards = document.querySelector("#container");
+  
+    fiveDayCards.innerHTML = fiveDayPullInfo;
+}
 document.querySelector("#searchForm").addEventListener("submit", function(event) {event.preventDefault()
   console.log(document.querySelector("#searchCity").value)
    weather.search()
+   generateCards();
 })
-
-//for loop
-// const temp = data.list[0].main.temp;
-
-
- // fetch5Day: function (city) {
-  //   fetch(
-  //     "api.openweathermap.org/data/2.5/forecast?lat=" +
-  //       lat +
-  //       "&lon=" +
-  //       lon +
-  //       "&appid=e57070c21ce9424475afa701ad71a404"
-  //   )
-  //     .then((response) => response.json())
-  //     .then((data) => this.display5Day(data));
-  // },
-  // display5Day: function (data) {
-  //   const { temp, humidity } = data.list[0].main;
-  // },
 
